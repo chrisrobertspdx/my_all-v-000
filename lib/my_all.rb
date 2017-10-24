4d4cd84d8fbdd1769a01e955da597e192cbe28bb
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-24 19:00:18 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,11 +2,12 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
+  returnArr = []
   while i < collection.size
     returnArr << yield(collection[i])
     i += 1
   end
-  if collection.include?(false)
+  if returnArr.include?(false)
     return false
   else
     return true
