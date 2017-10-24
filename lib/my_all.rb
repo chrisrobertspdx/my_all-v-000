e995ff0d50a6effb6bf3a6a3546be9016169fab1
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-24 18:59:09 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  while i < collection.size
+    returnArr << yield(collection[i])
+    i += 1
+  end
+  if collection.include(false)
+    return false
+  else
+    return true
 end
